d4d0ba0dda0213fea2bad950d401dd0a86a5b9ed
michaelk821 <101637657+michaelk821@users.noreply.github.com>
2022-04-19 19:44:22 +0800

    Correction for redefinition of struct error
    error fixed

--- player.cpp
+++ player.cpp
@@ -5,40 +5,27 @@
 
 using namespace std;
 
-#define MAX_HP 100
-#define DEFAULT_ATK 2
-#define DEFAULT_DEF 2
-
-struct Player {
-    int x = 0;
-    int y = 0;
-    int level = 0;
-    int hp = MAX_HP;
-    int atk = DEFAULT_ATK;
-    int def = DEFAULT_DEF;
-
-    void loadPlayer() {
-        ifstream fin;
-        fin.open("playerdata.txt");
-        if (fin.fail()) {
-            cout << "Error in file opening" << endl;
-            exit(1);
-        }
-        fin >> this->level >> this->hp >> this->atk >> this->def;
-        fin.close();
+void Player::loadPlayer() {
+    ifstream fin;
+    fin.open("playerdata.txt");
+    if (fin.fail()) {
+        cout << "Error in file opening" << endl;
+        exit(1);
     }
+    fin >> this->level >> this->hp >> this->atk >> this->def;
+    fin.close();
+}
 
-    void savePlayer() {
-        ofstream fout;
-        fout.open("playerdata.txt");
-        if (fout.fail()) {
-            cout << "Error in file opening" << endl;
-            exit(1);
-        }
-        fout << this->level << endl;
-        fout << this->hp << endl;
-        fout << this->atk << endl;
-        fout << this->def << endl;
-        fout.close();
+void Player::savePlayer() {
+    ofstream fout;
+    fout.open("playerdata.txt");
+    if (fout.fail()) {
+        cout << "Error in file opening" << endl;
+        exit(1);
     }
-};
+    fout << this->level << endl;
+    fout << this->hp << endl;
+    fout << this->atk << endl;
+    fout << this->def << endl;
+    fout.close();
+}
--- player.h
+++ player.h
@@ -1,13 +1,17 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#define MAX_HP 100
+#define DEFAULT_ATK 2
+#define DEFAULT_DEF 2
+
 struct Player {
-    int x;
-    int y;
-    int level;
-    int hp ;
-    int atk;
-    int def;
+    int x = 0;
+    int y = 0;
+    int level = 0;
+    int hp = MAX_HP;
+    int atk = DEFAULT_ATK;
+    int def = DEFAULT_DEF;
     void loadPlayer();
     void savePlayer();
 };
