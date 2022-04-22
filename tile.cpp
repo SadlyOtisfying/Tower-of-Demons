3e5d2693039dedce2b33ccf87fde8fd0df1aa204
michaelk821 <101637657+michaelk821@users.noreply.github.com>
2022-04-22 19:56:15 +0800

    implement constructor, getType, generate

--- tile.cpp
+++ tile.cpp
@@ -1,15 +1,37 @@
 #include "tile.h"
 
-Tile::Tile(int level, string type) {
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
+using namespace std;
+
+Tile::Tile(int level, string type) {
+    this->level = level;
+    this->type = type;
 }
 
 string Tile::getType() {
-
+    return type;
 }
 
+//generate value of tile
 void Tile::generate() {
-
+    srand(time(NULL));
+    if (type == "POTION") {
+        value = rand() % 10 + 10;
+    }
+    else if (type == "SWORD") {
+        value = (rand() % 5 + 3) * level;
+    }
+    else if (type == "ARMOR") {
+        value = (rand() % 5 + 5) * level;
+    }
+    else if (type == "DEMON") {
+        hp = (rand() % 5 + 5) * level;
+        atk = (rand() % 4 + 1) * level;
+        def = (rand() % 5 + 5) * level;
+    }
 }
 
 void Tile::battle() {
@@ -17,5 +39,5 @@ void Tile::battle() {
 }
 
 void Tile::removeItem() {
-    
+    type = "SPACE";
 }
