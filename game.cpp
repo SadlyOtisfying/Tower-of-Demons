874b183ed64b918d9296f6d4e61c097e4fc4fac3
michaelk821 <101637657+michaelk821@users.noreply.github.com>
2022-04-22 23:57:44 +0800

    implement prompt function

--- game.cpp
+++ game.cpp
@@ -1,15 +1,34 @@
 #include "player.h"
+#include "tile.h"
 
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
+//prompt the user for an integer with limited options
+int prompt(string output, int options) {
+    int ans;
+    cout<<output;
+    string input;
+    cin>>input;
+    try {
+        ans = stoi(input);
+        if(ans < 0 || ans >= options)
+            throw 1;
+    }
+    catch (...) {
+        cout<<"Invalid input. The last option will be chosen ;)"<<endl;
+        ans = options -1;
+    }
+    return ans;
+}
+
 int main() {
-    struct Player p;
-    cout<<p.hp<<endl;
-    p.savePlayer();
-
-
+    cout<<fixed<<setprecision(2);
+    cout<<"Welcome to Tower of Demons!"<<endl;
+    int diff = prompt("Please choose the difficulty (0 - easy; 1 - normal; 2 - hard): ", 3);
+    cout<<"Your love, Princess Lily was captured by some demons and locked in the top of a tower. Now, save her. Good luck."<<endl;
 
 }
 
