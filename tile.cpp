792d9a40a286ed8450770822ac60d7303761cb11
SadlyOtisfying <sadlyotisfying@gmail.com>
2022-04-22 19:07:56 +0800

    added empty functions for struct Tile

---
+++ tile.cpp
@@ -0,0 +1,17 @@
+#include <tile.h>
+
+void Tile::removeItem() {
+    type = "SPACE";
+}
+
+Tile::Tile(int level, string type) {
+
+}
+
+void Tile::item::generate() {
+
+}
+
+void Tile::item::generate() {
+    
+}
--- tile.h
+++ tile.h
@@ -8,14 +8,14 @@ using namespace std;
 struct Tile {
     string type = "";
     void removeItem();
-    bool isItem();
-    bool isDemon();
-    Tile();
+    Tile(int level, string type);
     struct item {
         int value;
+        void generate();
     };
     struct demon {
         int hp, atk, def;
+        void generate();
     };
 };
 
